6c8281a2dedea75abed4dd448f7a92f57b1cae26
ggadipuu <ggadipuu@brocade.com>
2016-12-10 06:16:35 -0800

    evpn-rbridge-id-fix seen on SQA setup

--- actions/configure_evpn_instance.py
+++ actions/configure_evpn_instance.py
@@ -29,10 +29,7 @@ class ConfigureEvpnInstance(NosDeviceAction):
         changes = {}
         with self.mgr(conn=self.conn, auth=self.auth) as device:
             self.logger.info('successfully connected to %s', self.host)
-            if type(rbridge_id) is list:
-                rbridge_id = rbridge_id[0]
-
-            if rbridge_id is None:
+            if rbridge_id is None or rbridge_id[0] is None:
                 rb_list = self._vlag_pair(device)
             else:
                 rb_list = rbridge_id
