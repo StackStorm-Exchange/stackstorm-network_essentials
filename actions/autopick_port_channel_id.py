06e7fca6095cad27dd01c96273cb046a70274999
ggadipuu <ggadipuu@brocade.com>
2016-12-09 18:17:47 -0800

    autopick port-channel action and make sure the changes reflect in workflow

---
+++ actions/autopick_port_channel_id.py
@@ -0,0 +1,56 @@
+# Copyright 2016 Brocade Communications Systems, Inc.
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from ne_base import NosDeviceAction
+
+
+class AutoPickPortChannel(NosDeviceAction):
+    """
+       Implements the logic to create port-channel on an interface on VDX Switches .
+       This action acheives the below functionality
+           1.Provides a port_channel number if port_channel id is not passed
+    """
+
+    def run(self, mgmt_ip, username, password, port_channel_id):
+        """Run helper methods to implement the desired state.
+        """
+
+        self.setup_connection(host=mgmt_ip, user=username, passwd=password)
+        changes = {}
+        with self.mgr(conn=self.conn, auth=self.auth) as device:
+            self.logger.info('successfully connected to %s to create port channel', self.host)
+            if not port_channel_id:
+                changes['port_channel_id'] = str(self._no_port_channel_number(device))
+            self.logger.info('closing connection to %s after'
+                             ' configuring port channel -- all done!', self.host)
+        return changes
+
+    def _no_port_channel_number(self, device):
+        """ Port channel number is Null , provide a number between 1-6144
+            which is not pre-existing
+        """
+        po_array = []
+        try:
+            result = device.interface.port_channels
+        except Exception, e:
+            raise ValueError(e)
+        for res in result:
+            port_channel_name = res['interface-name']
+            port_channel_num = port_channel_name.split("-")
+            po_array.append(port_channel_num[-1])
+        po_array = [int(i) for i in po_array]
+        for num in range(1, 6144):
+            if num not in po_array:
+                break
+        return num
