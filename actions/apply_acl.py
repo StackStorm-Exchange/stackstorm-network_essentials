b2ca7e2d2d070c4aa57b6a02fe78c39663420d87
prajwal <tprajwal@stratapps.com>
2016-12-21 18:11:41 -0800

    Jira: https://stackstorm.atlassian.net/browse/CANDY-107 As an administrator, need the ability to apply an L2 or L3 ACL to physical port, port channel, VE or management interface on a switch so that a I can allow or deny certain type of traffic. Must support following input parameters, required parameters are in bold. mgmt_ip: ip_type acl_name: string direction: in | out type switched | routed interface: interface_type | VE | Mgmt | <overlay-gateway name> interface_name: one or more comma separated interfaces Output parameters: TRUE for Success, FALSE for Failure

--- actions/apply_acl.py
+++ actions/apply_acl.py
@@ -1,9 +1,9 @@
-from base1 import DeviceAction
+from ne_base import NosDeviceAction
 
 
-class apply_acl(DeviceAction):
+class APPLY_ACL(NosDeviceAction):
     def run(self, mgmt_ip, username, password, intf_type, intf_name,
-            rbridge_id, acl_name, acl_direction, acl_type):
+            rbridge_id, acl_name, acl_direction, traffic_type):
         """Run helper methods to apply ACL on desired interface.
         """
         self.setup_connection(host=mgmt_ip, user=username, passwd=password)
@@ -16,13 +16,14 @@ class apply_acl(DeviceAction):
             self.logger.info('successfully connected to %s to enable interface', self.host)
         except Exception as e:
             raise ValueError('Failed to connect to %s due to %s', self.host, e.message)
-
+        ag_type = self._get_acl_type_(device, acl_name)['protocol']
+        self.logger.info('successfully identified the access group type as %s', ag_type)
         # Check is the user input for Interface Name is correct
         for intf in intf_name:
             if "-" not in intf:
                 interface_list.append(intf)
             else:
-                ex_intflist = self.expand_interface_range(intf_type=intf_type, intf_name=intf)
+                ex_intflist = self.extend_interface_range(intf_type=intf_type, intf_name=intf)
                 for ex_intf in ex_intflist:
                     interface_list.append(ex_intf)
         msg = None
@@ -36,45 +37,41 @@ class apply_acl(DeviceAction):
                                       rbridge_id=rbridge_id,
                                       acl_name=acl_name,
                                       acl_direction=acl_direction,
-                                      acl_type=acl_type
-                                      )
+                                      ag_type=ag_type,
+                                      traffic_type=traffic_type)
         else:
             raise ValueError(msg)
         output['result'] = changes
-        self.logger.info('closing connection to %s after applying access-list-- \
+        self.logger.info('closing connection to %s after removing access-list-- \
                       all done!', self.host)
         return output
 
     def _apply_acl(self, device, intf_type, intf_name, rbridge_id,
-                   acl_name, acl_direction, acl_type):
+                   acl_name, acl_direction, ag_type, traffic_type):
         result = []
         for intf in intf_name:
-            if acl_type == 'routed':
-                atype = 'ip'
-            elif acl_type == 'switched':
-                atype = 'mac'
-            elif acl_type == 'routed_ipv6':
-                atype = 'ipv6'
             method = 'rbridge_id_interface_{}_{}_access_group_create'. \
-                format(intf_type, atype) if rbridge_id \
-                else 'interface_{}_{}_access_group_create'.format(intf_type, atype)
+                format(intf_type, ag_type) if rbridge_id \
+                else 'interface_{}_{}_access_group_create'.format(intf_type, ag_type)
+
             aply_acl = eval('device.{}'.format(method))
-            access_grp = (str(acl_name), str(acl_direction), str(acl_type))
+            access_grp = (acl_name, acl_direction, traffic_type)
+            print access_grp
             self.logger.info('Applying ACL %s on int-type - %s int-name- %s',
                              acl_name, intf_type, intf)
             try:
-                aply = list(aply_acl(rbridge_id, intf, access_grp) if rbridge_id else
-                            list(aply_acl(intf, access_grp)))
+                aply = list(aply_acl(rbridge_id, intf, access_grp)
+                            if rbridge_id else list(aply_acl(intf, access_grp)))
                 result.append(str(aply[0]))
                 if not eval(str(aply[0])):
-                    self.logger.info('Cannot apply  %s on interface %s %s due to %s',
-                                     acl_name, intf_type, intf,
-                                     str(aply[1][0][self.host]['response']['json']['output']))
+                    self.logger.info('Cannot apply  %s on interface %s %s due to %s', acl_name,
+                                     intf_type, intf, str(aply[1][0][self.host]
+                                                          ['response']['json']['output']))
                 else:
                     self.logger.info('Successfully  applied  %s ACL on interface %s %s ',
                                      acl_name, intf_type, intf)
             except Exception as e:
-                self.logger.info('Cannot apply  %s on interface %s %s due to %s',
+                self.logger.info('Cannot apply %s on interface %s %s due to %s',
                                  acl_name, intf_type, intf, e.message)
                 raise ValueError(e.message)
         return result
--- actions/ne_base.py
+++ actions/ne_base.py
@@ -18,6 +18,8 @@ import re
 import ipaddress
 import pynos.device
 import pynos.utilities
+import pyswitchlib.asset
+import requests.exceptions
 from st2actions.runners.pythonrunner import Action
 
 
@@ -30,6 +32,9 @@ class NosDeviceAction(Action):
         self.host = None
         self.conn = None
         self.auth = None
+        self.asset = pyswitchlib.asset.Asset
+        self.RestInterfaceError = pyswitchlib.asset.RestInterfaceError
+        self.ConnectionError = requests.exceptions.ConnectionError
 
     def setup_connection(self, host, user=None, passwd=None):
         self.host = host
@@ -119,6 +124,44 @@ class NosDeviceAction(Action):
 
         return vlan_id
 
+    def validate_interface(self, intf_type, intf_name, rbridge_id=None):
+        msg = None
+        # int_list = intf_name
+        re_pattern1 = r"^(\d+)$"
+        re_pattern2 = r"^(\d+)\/(\d+)\/(\d+)$"
+        re_pattern3 = r"^(\d+)\/(\d+)$"
+        intTypes = ["port_channel", "gigabitethernet", "tengigabitethernet",
+                    "fortygigabitethernet", "hundredgigabitethernet", "ethernet"]
+        NosIntTypes = ["gigabitethernet", "tengigabitethernet", "fortygigabitethernet"]
+        if rbridge_id is None and 'loopback' in intf_type:
+            msg = 'Must specify `rbridge_id` when specifying a `loopback`'
+        elif rbridge_id is None and 've' in intf_type:
+            msg = 'Must specify `rbridge_id` when specifying a `ve`'
+        elif rbridge_id is not None and intf_type in intTypes:
+            msg = 'Should not specify `rbridge_id` when specifying a ' + intf_type
+        elif re.search(re_pattern1, intf_name):
+            intf = intf_name
+        elif re.search(re_pattern2, intf_name) and intf_type in NosIntTypes:
+            intf = intf_name
+        elif re.search(re_pattern3, intf_name) and 'ethernet' in intf_type:
+            intf = intf_name
+        else:
+            msg = 'Invalid interface format'
+
+        if msg is not None:
+            self.logger.info(msg)
+            return False
+
+        intTypes = ["ve", "loopback", "ethernet"]
+        if intf_type not in intTypes:
+            tmp_vlan_id = pynos.utilities.valid_interface(intf_type, name=str(intf))
+
+            if not tmp_vlan_id:
+                self.logger.info("Not a valid interface type %s or name %s", intf_type, intf)
+                return False
+
+        return True
+
     def expand_interface_range(self, intf_type, intf_name, rbridge_id):
         msg = None
 
@@ -161,7 +204,7 @@ class NosDeviceAction(Action):
             int_list = []
             for intf in intList:
                 int_list.append(temp_list.groups()[0] + '/' + temp_list.groups()[1] + '/' +
-                 str(intf))
+                                str(intf))
             int_list = int_list
         else:
             msg = 'Invalid interface format'
@@ -178,6 +221,61 @@ class NosDeviceAction(Action):
                 if not tmp_vlan_id:
                     self.logger.info("Not a valid interface type %s or name %s", intf_type, intf)
                     return None
+
+        return int_list
+
+    def extend_interface_range(self, intf_type, intf_name):
+        msg = None
+
+        int_list = intf_name
+        re_pattern1 = r"^(\d+)\-?(\d+)$"
+        re_pattern2 = r"^(\d+)\/(\d+)\-?(\d+)$"
+        re_pattern3 = r"^(\d+)\/(\d+)\/(\d+)\-?(\d+)$"
+
+        if re.search(re_pattern1, int_list):
+            try:
+                int_list = re.match(re_pattern1, int_list)
+            except Exception:
+                return None
+
+            if int(int_list.groups()[0]) == int(int_list.groups()[1]):
+                self.logger.info("Use range command only for unique values")
+            int_list = range(int(int_list.groups()[0]), int(int_list.groups()[1]) + 1)
+
+        elif re.search(re_pattern2, int_list):
+            try:
+                temp_list = re.match(re_pattern2, int_list)
+            except Exception:
+                return None
+
+            if int(temp_list.groups()[1]) == int(temp_list.groups()[2]):
+                self.logger.info("Use range command only for unique values")
+            intList = range(int(temp_list.groups()[1]), int(temp_list.groups()[2]) + 1)
+            int_list = []
+            for intf in intList:
+                int_list.append(temp_list.groups()[0] + '/' + str(intf))
+            int_list = int_list
+
+        elif re.search(re_pattern3, int_list):
+            try:
+                temp_list = re.match(re_pattern3, int_list)
+            except Exception:
+                return None
+
+            if int(temp_list.groups()[2]) == int(temp_list.groups()[3]):
+                self.logger.info("Use range command only for unique values")
+            intList = range(int(temp_list.groups()[2]), int(temp_list.groups()[3]) + 1)
+            int_list = []
+            for intf in intList:
+                int_list.append(temp_list.groups()[0] + '/' + temp_list.groups()[1] + '/' +
+                                str(intf))
+            int_list = int_list
+        else:
+            msg = 'Invalid interface format'
+
+        if msg is not None:
+            self.logger.info(msg)
+            return None
 
         return int_list
 
