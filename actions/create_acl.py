5e211fb8e3bea13a8276144f9582a1bce8aa2aaa
prajwal <tprajwal@stratapps.com>
2016-12-22 12:20:10 -0800

    JIRA: https://stackstorm.atlassian.net/browse/CANDY-126 As an administrator, need the ability to create an L2 or L3 ACL on a switch so that a I can allow or deny certain type of traffic. Must support following input parameters, required parameters are in bold. mgmt_ip: ip_type acl_name: string type : switched | routed Output parameters: TRUE for Success, FALSE for Failure

---
+++ actions/create_acl.py
@@ -0,0 +1,60 @@
+from ne_base import NosDeviceAction
+
+
+class CreateAcl(NosDeviceAction):
+    """
+    Creating ipv4 and ipv6 ACLs
+    """
+
+    def run(self, mgmt_ip, username, password, acl_type, acl_name):
+        self.setup_connection(host=mgmt_ip, user=username, passwd=password)
+        changes = {}
+        try:
+            device = self.asset(ip_addr=self.host, auth=self.auth)
+            self.logger.info('successfully connected to %s to create ACL',
+                             self.host)
+        except AttributeError as e:
+            raise ValueError('Failed to connect to %s due to %s', self.host, e.message)
+        except ValueError as verr:
+            self.logger.error("Error while logging in to %s due to %s",
+                              self.host, verr.message)
+            raise ValueError("Error while logging in to %s due to %s",
+                             self.host, verr.message)
+        except self.ConnectionError as cerr:
+            self.logger.error("Connection failed while logging in to %s due to %s",
+                              self.host, cerr.message)
+            raise ValueError("Connection failed while logging in to %s due to %s",
+                             self.host, cerr.message)
+        except self.RestInterfaceError as rierr:
+            self.logger.error("Failed to get a REST response while logging in "
+                              "to %s due to %s", self.host, rierr.message)
+            raise ValueError("Failed to get a REST response while logging in "
+                             "to %s due to %s", self.host, rierr.message)
+        changes = self._create_acl(device, acl_type, acl_name)
+        return changes
+
+    def _create_acl(self, device, acl_type, acl_name):
+        create = []
+        result = {}
+        if acl_type == 'extended':
+            atype = 'extended'
+        elif acl_type == 'standard':
+            atype = 'standard'
+        else:
+            self.logger.info('Invalid acl_type %s', acl_type)
+            return result
+        method = 'ip_access_list_{}_create'.format(atype)
+        create_acl = eval('device.{}'.format(method))
+        self.logger.info('Creating ACL %s', acl_name)
+        try:
+            create = create_acl(acl_name)
+            if not create[0]:
+                self.logger.info('Cannot create ACL %s due to %s', acl_name,
+                                 str(create[1][0][self.host]['response']['json']['output']))
+            else:
+                self.logger.info('Successfully created ACL %s in %s', acl_name, self.host)
+        except (KeyError, ValueError, AttributeError) as e:
+            self.logger.info('Cannot create ACl %s due to %s', acl_name, e.message)
+            raise ValueError(e.message)
+        result['result'] = create[0]
+        return result
