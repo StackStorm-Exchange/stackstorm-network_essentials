154b17c11e21b21d68617808a86de9646ffd166d
tprajwal <tprajwal@stratapps.com>
2017-01-04 17:17:07 -0800

    Update add_ipv4_rule_acl.py
    correction based on 1st review.

--- actions/add_ipv4_rule_acl.py
+++ actions/add_ipv4_rule_acl.py
@@ -12,6 +12,7 @@ class Add_Ipv4_Rule_Acl(NosDeviceAction):
             device = self.asset(ip_addr=self.host, auth=self.auth)
             self.logger.info('successfully connected to %s to enable interface', self.host)
         except AttributeError as e:
+            self.logger.error("Failed to connect to %s due to %s", self.host, e.message)
             raise ValueError('Failed to connect to %s due to %s', self.host, e.message)
         except ValueError as verr:
             self.logger.error("Error while logging in to %s due to %s",
@@ -50,28 +51,35 @@ class Add_Ipv4_Rule_Acl(NosDeviceAction):
             acl_type = self._get_acl_type_(device, acl_name)['type']
             self.logger.info('successfully identified the acl_type as %s', acl_type)
         except:
+            self.logger.error('cannot get acl_type')
             raise ValueError('cannot get acl_type')
         if acl_type == 'standard':
             seq_variables = seq_variables_std
         elif acl_type == 'extended':
             seq_variables = seq_variables_ext
         if acl_type == 'extended' and destination is None:
+            self.logger.error('Destination required in extended access list')
             raise ValueError('Destination required in extended access list')
         elif acl_type == 'extended' and protocol_type is None:
+            self.logger.error('protocol_type is required for extended access list')
             raise ValueError('protocol_type is required for extended access list')
         elif acl_type == 'standard' and destination:
+            self.logger.error('Destination cannot be given for standard access list')
             raise ValueError('Destination cannot be given for standard access list')
         elif acl_type == 'standard' and protocol_type:
+            self.logger.error('protocol_type cannot be given for standard access list')
             raise ValueError('protocol_type cannot be given for standard access list')
         try:
             seq_dict = {key: None for key in seq_variables}
         except:
+            self.logger.error('Cannot get seq_variables')
             raise ValueError('Cannot get seq_variables')
 
         if seq_id is None:
             self.logger.info('seq_id not provided getting the seq_id')
             seq_id = self._get_seq_id_(device, acl_name, acl_type)
             if seq_id is None:
+                self.logger.error('Cannot get seq_id')
                 raise ValueError('Cannot get seq_id')
         self.logger.info('seq_id for the rule is %s', seq_id)
         src_dict = self._parse_(protocol_type, source, 'src')
@@ -105,6 +113,7 @@ class Add_Ipv4_Rule_Acl(NosDeviceAction):
                                           acl_type=acl_type,
                                           seq=tuple(seq))
         except Exception as msg:
+            self.logger.error(msg)
             raise ValueError(msg)
         output['result'] = changes
         self.logger.info('closing connection to %s after adding rule access-list-- '
@@ -138,6 +147,7 @@ class Add_Ipv4_Rule_Acl(NosDeviceAction):
         else:
             msg = 'Incorrect {} statement'.format(key)
         if msg is not None:
+            self.logger.error(msg)
             raise ValueError(msg)
         try:
             port = statement_list[0]
@@ -167,6 +177,7 @@ class Add_Ipv4_Rule_Acl(NosDeviceAction):
             msg = 'Incorrect {} statement'.format(key)
 
         if msg is not None:
+            self.logger.error(msg)
             raise ValueError(msg)
         return output
 
@@ -179,6 +190,9 @@ class Add_Ipv4_Rule_Acl(NosDeviceAction):
                 add_acl = device.ip_access_list_standard_seq_create
             elif acl_type == 'extended':
                 add_acl = device.ip_access_list_extended_seq_create
+            else:
+                self.logger.error('Invalid access list type %s', acl_type)
+                raise ValueError('Invalid access list type %s', acl_type)
             aply = list(add_acl(acl_name, seq))
             result = str(aply[0])
             if str(aply[0]) == 'False':
