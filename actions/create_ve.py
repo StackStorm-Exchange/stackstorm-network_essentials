00d7daf705d15bcd2eac91afcdc1ee8c2aee6364
bharatu <bharatu@brocade.com>
2017-03-22 04:58:07 +0530

    mandated the format ip_address/subnet

--- actions/create_ve.py
+++ actions/create_ve.py
@@ -44,6 +44,8 @@ class CreateVe(NosDeviceAction):
             self.logger.info('successfully connected to %s to create'
                              ' Ve', self.host)
             if device.os_type == 'nos':
+                if rbridge_id is None:
+                    rbridge_id = self.vlag_pair(device)
                 if ip_address is None:
                     tmp_list = rbridge_id
                 else:
@@ -180,7 +182,7 @@ class CreateVe(NosDeviceAction):
     def _check_requirements_ve(self, device, ve_name, rbridge_id):
         """ Verify if the VE is pre-existing """
 
-        ves = device.interface.ve_interfaces(rbridge_id=rbridge_id)
+        ves = device.interface.ve_interfaces()
         for each_ve in ves:
             tmp_ve_name = 'Ve ' + ve_name
             if each_ve['if-name'] == tmp_ve_name:
@@ -194,15 +196,19 @@ class CreateVe(NosDeviceAction):
                                rbridge_id, vrf_name):
         """ Verify if the ip address is already associated to the VE """
 
+        '''
         try:
             ip_tmp = ip_interface(unicode(ip_address))
             ip_address = ip_tmp.with_prefixlen
         except ValueError:
             self.logger.info('Invalid IP address %s', ip_address)
-
+        '''
         if len(unicode(ip_address).split("/")) != 2:
             raise ValueError('Pass IP address along with netmask.'
                              '(ip-address/netmask)', ip_address)
+        tmp_ip = unicode(ip_address).split("/")[0]
+        if not self.is_valid_ip(tmp_ip):
+            raise ValueError('Invalid IP address %s', tmp_ip)
 
         ves = device.interface.ve_interfaces(rbridge_id=rbridge_id)
         for each_ve in ves:
@@ -369,7 +375,7 @@ class CreateVe(NosDeviceAction):
                                          vrf_name=vrf_name)
         except (ValueError, KeyError):
             self.logger.info('Invalid Input values while configuring VRF %s on'
-                             ' Ve %s on rbridge-id %s', vrf_name, ve_name,
+                             'Ve %s on rbridge-id %s', vrf_name, ve_name,
                              rbridge_id)
 
     def _admin_state(self, device, ve_name, rbridge_id):
