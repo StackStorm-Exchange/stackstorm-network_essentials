a43839948149ee98470f9c63d548843308a2aa85
uday <bharatu@brocade.com>
2017-07-17 14:52:31 -0700

    Pushing the code. Will verify once the REST INFRA is fixed

--- actions/configure_mac_group.py
+++ actions/configure_mac_group.py
@@ -42,10 +42,14 @@ class ConfigureMacGroup(NosDeviceAction):
                 'successfully connected to %s to Configure Mac Group'
                 ' on the device', self.host)
 
-            # self._validate_inputs(mac_group_id, mac_address)
-            changes['pre_check_group'] = self._pre_check_mac_group(device, mac_group_id)
+            self._validate_inputs(mac_group_id, mac_address)
+            changes['pre_check_group'], macs = self._pre_check_mac_group(device, mac_group_id)
             if changes['pre_check_group']:
                 changes['configure_mac_group'] = self._config_mac_group(device, mac_group_id)
+            if mac_address is not None:
+                changes['configure_entry_macs'] = self._config_entry_macs(device,
+                                                                          mac_group_id,
+                                                                          entry_macs=mac_address)
 
             self.logger.info('Closing connection to %s after Configuring Mac Group'
                              'on the device -- all done!',
@@ -55,9 +59,10 @@ class ConfigureMacGroup(NosDeviceAction):
     def _validate_inputs(self, mac_group_id, mac_address):
         """ Check if inputs are valid or not """
 
-        for each_mac in mac_address:
-            if not self.is_valid_mac(each_mac):
-                raise ValueError('Invalid MAC Address %s', each_mac)
+        if mac_address is not None:
+            for each_mac in mac_address:
+                if not self.is_valid_mac(each_mac):
+                    raise ValueError('Invalid MAC Address %s', each_mac)
 
         if mac_group_id not in range(1, 501):
             raise ValueError('Invalid MAC Group Id %s', mac_group_id)
@@ -69,11 +74,11 @@ class ConfigureMacGroup(NosDeviceAction):
 
         out = device.interface.mac_group_create(get=True, mac_group_id=mac_group_id)
 
-        if out is not None:
+        if out is not None and int(out) == mac_group_id:
             self.logger.info('Mac Group %s is pre-existing on the device', mac_group_id)
-            return False
+            return False, out
 
-        return True
+        return True, out
 
     def _config_mac_group(self, device, mac_group_id):
         """ Configure mac group """
@@ -84,5 +89,21 @@ class ConfigureMacGroup(NosDeviceAction):
         except (ValueError, KeyError):
             self.logger.exception("Configuring Mac Group Failed", mac_group_id)
             raise ValueError("Configuring Mac Group Failed")
+
+        return True
+
+    def _config_entry_macs(self, device, mac_group_id, entry_macs):
+        """ Configure Entry Mac Address for mac group """
+
+        try:
+            self.logger.info('Configuring Entry Mac Address %s for Mac Group %s ',
+                             entry_macs, mac_group_id)
+            for each_mac in entry_macs:
+                device.interface.mac_group_mac_create(mac_group_id=mac_group_id,
+                                                      mac_address=each_mac)
+        except (ValueError, KeyError):
+            self.logger.exception('Configuring Entry Mac Address %s for Mac Group %s Failed',
+                                  each_mac, mac_group_id)
+            raise ValueError("Configuring Mac Group Entry Mac Address Failed")
 
         return True
