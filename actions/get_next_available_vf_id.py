3741e14961214f8a330fc03556a97b86fcc76faf
bharatu <bharatu@brocade.com>
2017-08-22 17:04:00 +0530

    CANDY-645: Action to return single/list of VF ids

--- actions/get_next_available_vf_id.py
+++ actions/get_next_available_vf_id.py
@@ -14,6 +14,7 @@
 
 from ne_base import NosDeviceAction
 from ne_base import log_exceptions
+import re
 
 MAX_DOT1Q_VLAN = 4095
 
@@ -26,16 +27,16 @@ class AutoPickPortChannel(NosDeviceAction):
            1.Provides a vfab number if vfab or network id is not passed
     """
 
-    def run(self, mgmt_ip, username, password):
+    def run(self, mgmt_ip, username, password, length_of_the_range):
         """Run helper methods to implement the desired state.
         """
 
         self.setup_connection(host=mgmt_ip, user=username, passwd=password)
-        changes = self.switch_operation()
+        changes = self.switch_operation(length_of_the_range)
         return changes
 
     @log_exceptions
-    def switch_operation(self):
+    def switch_operation(self, length_of_the_range):
         changes = {}
         with self.pmgr(conn=self.conn, auth=self.auth) as device:
             if device.os_type != 'nos':
@@ -45,24 +46,42 @@ class AutoPickPortChannel(NosDeviceAction):
                 'successfully connected to %s to fetch vfab id or network id',
                 self.host)
             changes['vf_id'] = str(
-                self._no_vfab_number(device))
+                self._no_vfab_number(device, length_of_the_range))
             self.logger.info('closing connection to %s after'
                              ' autopicking vfab or network id  -- all done!',
                              self.host)
         return changes
 
-    def _no_vfab_number(self, device):
+    def _no_vfab_number(self, device, length_of_the_range):
         """ vfab number is Null , provide a number between 1-6144
             which is not pre-existing
         """
         vfab_array = []
         vfab_range = xrange(4096, 8192)
+        re_pattern1 = r"^(\d+)$"
+        re_pattern2 = r"^(\d+)\-(\d+)$"
+
+        if re.search(re_pattern2, length_of_the_range):
+            length = len(range(int(length_of_the_range.split('-')[0]),
+                               int(length_of_the_range.split('-')[1]) + 1))
+            length_of_the_range = int(length)
+        elif re.search(re_pattern1, length_of_the_range):
+            length_of_the_range = int(length_of_the_range)
+        else:
+            self.logger.error('Invalid format in args `length_of_the_range`')
+            raise ValueError('Invalid format in args `length_of_the_range`')
+
         try:
             vfab_mode = device.interface.vfab_enable(get=True)
             if not vfab_mode:
-                self.logger.info('vfab mode is disabled Hence autopicking'
+                self.logger.info('vfab mode is disabled, hence autopicking'
                                  ' is not possible')
                 return None
+            if int(length_of_the_range) not in xrange(1, 4096):
+                self.logger.error('length_of_the_range %s must be a value between 1 to 4095',
+                                  length_of_the_range)
+                raise ValueError('length_of_the_range %s must be a value between 1 to 4095' %
+                                 (length_of_the_range))
             result = device.interface.vlans
         except Exception as e:
             raise ValueError(e)
@@ -71,10 +90,30 @@ class AutoPickPortChannel(NosDeviceAction):
             if vfab_num < MAX_DOT1Q_VLAN + 1:
                 continue
             vfab_array.append(vfab_num)
-        for num in vfab_range:
-            if num not in vfab_array:
-                break
-            elif num == 8191:
-                self.logger.info('No free VF ID available on the device')
-                num = ''
+
+        available_vlans_length = len(xrange(4096, 8192)) - len(vfab_array)
+        if available_vlans_length < length_of_the_range:
+            self.logger.error('Not enough VF IDs are available on the device for the range')
+            raise ValueError('Not enough VF IDs are available on the device for the range')
+
+        if length_of_the_range == 1:
+            for num in vfab_range:
+                if num not in vfab_array:
+                    break
+                elif num == 8191:
+                    self.logger.info('No free VF ID available on the device')
+                    num = ''
+        else:
+            tmp_list = []
+            available_vlans = set(xrange(4096, 8192)).symmetric_difference(set(vfab_array))
+            for numd in available_vlans:
+                if len(tmp_list) == length_of_the_range:
+                    break
+                tmp_list.append(numd)
+
+            # convert the list to string for the workflow
+            vlan_str = ''
+            for tmp in tmp_list:
+                vlan_str = vlan_str + ',' + str(tmp)
+            num = vlan_str.lstrip(',')
         return num
