eceeceac8e032cc3fa5f372a54985cc507d562d0
Amit Ray <aray@brocade.com>
2017-09-01 17:04:57 -0700

    Firmware download support

---
+++ actions/firmware.py
@@ -0,0 +1,91 @@
+# Copyright 2016 Brocade Communications Systems, Inc.
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from ne_base import NosDeviceAction
+from ne_base import log_exceptions
+from threading import Timer
+
+class Firmware(NosDeviceAction):
+    """
+    Implements the logic to download firmware on the switch and
+    check status.
+
+    """
+    fwdl_monitor_timer = None
+    last_proc_fwdl_entry = 0
+    fwdl_complete = False
+
+    def run(self, mgmt_ip, username, password, host_ip, protocol_type,
+            proto_username, proto_password, firmware_path):
+
+        self.setup_connection(host=mgmt_ip, user=username, passwd=password)
+
+        return self.switch_operation(host_ip, protocol_type,
+            proto_username, proto_password, firmware_path)
+
+    @log_exceptions
+    def switch_operation(self, host_ip, protocol_type, proto_username, proto_password, firmware_path):
+        with self.pmgr(conn=self.conn, auth=self.auth) as device:
+            self.logger.info('successfully connected to %s to download firmware', self.host)
+            fwdl_status_dict = device.firmware.download_firmware(protocol=protocol_type, host=host_ip,
+                                                                 user_name=proto_username,
+                                                                 password=proto_password,
+                                                                 directory=firmware_path)
+            self.logger.info("Download Status code: %d Status message:%s", fwdl_status_dict['status_code'],
+                             fwdl_status_dict['status_message'])
+            if fwdl_status_dict['status_code'] == 0:
+                """
+                Start Monitor process
+                """
+                self.last_proc_fwdl_entry = 0
+                self.fwdl_monitor_timer = Timer(5, lambda: self.firmware_download_monitor_periodic())
+                self.fwdl_monitor_timer.start()
+            else:
+                pass
+
+    def firmware_download_monitor_periodic(self):
+        with self.pmgr(conn=self.conn, auth=self.auth) as device:
+            self.fwdl_monitor_timer = None
+            fwdl_status_list = device.firmware.firmware_download_monitor()
+            for fwdl_status in fwdl_status_list:
+                index = fwdl_status['index']
+                if index <= self.last_proc_fwdl_entry:
+                    continue
+                else:
+                    self.last_proc_fwdl_entry = index
+                    self.logger.info("Index: %d Blade:%s Time:%s Message:%s", index, fwdl_status['blade-name'],
+                                     fwdl_status['timestamp'], fwdl_status['message'])
+                    if fwdl_status['message'] == 'Firmware is downloaded successfully.':
+                        self.logger.info("All done. Process complete")
+                        self.fwdl_complete = True
+                    else:
+                        pass
+            if self.fwdl_complete is True:
+                self.last_proc_fwdl_entry = 0
+                self.fwdl_monitor_timer = None
+            else:
+                self.fwdl_monitor_timer = Timer(5, lambda: self.firmware_download_monitor_periodic())
+                self.fwdl_monitor_timer.start()
+
+
+
+
+
+
+
+
+
+
+
+
