df0e429ca507a0429cbc0fbec99b116cf1fcfe09
Udaya Bharat <uday83@users.noreply.github.com>
2017-11-03 17:57:40 +0530

    Update create_l2_port_channel.py

--- actions/create_l2_port_channel.py
+++ actions/create_l2_port_channel.py
@@ -39,7 +39,7 @@ class CreatePortChannel(NosDeviceAction):
         if protocol == "modeon":
             protocol = "on"
 
-        with self.pmgr(conn=self.conn) as device:
+        with self.pmgr(conn=self.conn, auth_snmp=self.auth_snmp) as device:
             self.logger.info('successfully connected to %s to create port channel', self.host)
             if device.os_type == 'slxos':
                 if mode != "standard":
